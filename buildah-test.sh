4373a3d666939cd7035ae8c1914cbf468ead8401
Jose Montoya <jam01@protonmail.com>
2021-03-17 18:52:30 -0500

    test;

--- buildah-test.sh
+++ buildah-test.sh
@@ -6,4 +6,5 @@ set -o errexit
 builder=$(buildah from scratch)
 mount=$(buildah mount $builder)
 buildah unmount $builder
+buildah copy $builder requirements.txt /
 buildah commit --rm $builder localhost/test:latest
